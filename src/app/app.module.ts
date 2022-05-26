fb1b64e458be0bd1dabf83b6669f901c7de75e84
umitakbas82 <umit.akbas@hotmail.com.tr>
2022-05-27 00:44:18 +0300

    CrudApp On Angular13

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ContactManagerComponent } from './contact-manager/contact-manager.component';
+import { AddContactComponent } from './add-contact/add-contact.component';
+import { EditContactComponent } from './edit-contact/edit-contact.component';
+import { ViewContactComponent } from './view-contact/view-contact.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ContactManagerComponent,
+    AddContactComponent,
+    EditContactComponent,
+    ViewContactComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/models/IContact.ts
@@ -0,0 +1,10 @@
+export interface IContact {
+    id?:string
+    name:string
+    email:string
+    phone:string
+    mobile:string
+    company:string
+    title:string
+    groupId:string
+}
